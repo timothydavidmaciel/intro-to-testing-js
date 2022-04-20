d46cb10d7ba51dd41e897bca578b8cc6813e6769
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-04-20 15:24:18 -0500

    Intro to Testing exercise

--- code.js
+++ code.js
@@ -2,3 +2,7 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(x) {
+return "Hello " + x;
+}
--- tests.js
+++ tests.js
@@ -1,15 +1,30 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello " with the argument when executed', function () {
+        expect(sayHello("")).toBe("Hello ");
+    });
+    it("should never return 'undefined' when called", function () {
+        expect(sayHello()).not.toBe(undefined);
     });
 });
